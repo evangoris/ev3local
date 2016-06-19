be70ac06cdcb0b2344f8db857ff3525a498df3b5
Evan Goris <evan.goris@xillio.nl>
2016-06-19 14:13:54 +0200

    Move standalone code to samples folder

--- ev3local/httpserver.py
+++ ev3local/httpserver.py
@@ -1,21 +1,19 @@
 """Simple HTTP Server that can return the state of servos and sensors
 
 The devices and the ports to which they are connected need to be given
-at startup. For example if a servo is attached to port A and an infra-red
-sensor to port 3 then we can start the server as follows
+at construction. For example if a servo is attached to port A and an infra-red
+sensor to port 3 then we can construct a server as follows
 
-    python httpserver.py --outA=tachomotor --in3=infrared
+    Server(('0.0.0.0', 5000), RequestHandler(), devices={'outA': ev3.TachoMotor, 'in3': ev3.InfraRed})
 
 A client can then request the state of these devides with queries like
 
-   http://ev3dev:500/data?outA!Speed&outA!Position&in3!Proximity
+   http://ev3dev:5000/data?outA!Speed&outA!Position&in3!Proximity
 
 Evan Goris, 2016
 """
 import BaseHTTPServer
-
 import ev3local.xbox
-
 
 class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     """Handles requests for the state of sensors and servos
@@ -119,45 +117,4 @@ class Server(BaseHTTPServer.HTTPServer):
         properties = device.getreadproperties()
         return properties
 
-def main():
-    import argparse, ev3
 
-    ports = ['outA', 'outB', 'outC', 'outD', 'in1', 'in2', 'in3', 'in4']
-    constructors = {
-        'tachomotor': ev3.TachoMotor,
-        'infrared': ev3.Infrared_Sensor
-    }
-
-    parser = argparse.ArgumentParser(usage='', description='')
-    parser.add_argument('--xbox', action='store_true', default=False, dest='xbox', help='deliver xbox controller events')
-    for port in ports:
-        parser.add_argument('--%(p)s'%{'p': port}, type=str, dest=port, default=None, help='device attached to port %(p)s'%{'p': port[-1:]})
-    args = parser.parse_args()
-
-
-    devices = {}
-    for port in ports:
-        device = getattr(args, port)
-        if device:
-            devices[port] = constructors[device](port)
-
-    xcevents = None
-    if args.xbox:
-        import ev3local.xbox
-        xcevents = ev3local.xbox.XCEvents()
-        xcevents.__enter__()
-
-    try:
-        server = Server(("0.0.0.0", 500), RequestHandler, devices, xcevents)
-        server.serve_forever()
-    finally:
-        if xcevents:
-            xcevents.__exit__(None, None, None)
-
-if __name__=='__main__':
-    import socket
-    print socket.gethostname()
-    try:
-        main()
-    except KeyboardInterrupt:
-        pass
---
+++ samples/httpserver.py
@@ -0,0 +1,57 @@
+"""Simple HTTP Server that can return the state of servos and sensors
+
+The devices and the ports to which they are connected need to be given
+at startup. For example if a servo is attached to port A and an infra-red
+sensor to port 3 then we can start the server as follows
+
+    python httpserver.py --outA=tachomotor --in3=infrared
+
+A client can then request the state of these devides with queries like
+
+   http://ev3dev:500/data?outA!Speed&outA!Position&in3!Proximity
+
+Evan Goris, 2016
+"""
+def main():
+    from ev3local.httpserver import Server, RequestHandler
+    import argparse, ev3local.ev3 as ev3, ev3local.xbox
+
+    ports = ['outA', 'outB', 'outC', 'outD', 'in1', 'in2', 'in3', 'in4']
+    constructors = {
+        'tachomotor': ev3.TachoMotor,
+        'infrared': ev3.Infrared_Sensor
+    }
+
+    parser = argparse.ArgumentParser(usage='', description='')
+    parser.add_argument('--xbox', action='store_true', default=False, dest='xbox', help='deliver xbox controller events')
+    for port in ports:
+        parser.add_argument('--%(p)s'%{'p': port}, type=str, dest=port, default=None, help='device attached to port %(p)s'%{'p': port[-1:]})
+    args = parser.parse_args()
+
+
+    devices = {}
+    for port in ports:
+        device = getattr(args, port)
+        if device:
+            devices[port] = constructors[device](port)
+
+    xcevents = None
+    if args.xbox:
+        import ev3local.xbox
+        xcevents = ev3local.xbox.XCEvents()
+        xcevents.__enter__()
+
+    try:
+        server = Server(("0.0.0.0", 5000), RequestHandler, devices, xcevents)
+        server.serve_forever()
+    finally:
+        if xcevents:
+            xcevents.__exit__(None, None, None)
+
+if __name__=='__main__':
+    import socket
+    print socket.gethostname()
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
