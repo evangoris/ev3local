8efecd55283c210091987082591c0b87576f8dc0
Evan Goris <evan.goris@xillio.nl>
2016-07-17 12:58:41 +0200

    Some changes to XCEvents: Add connectaxes() method and rework resource management

--- samples/rc.py
+++ samples/rc.py
@@ -1,10 +1,12 @@
 import ev3local.pid
 
+import logging
+logging.basicConfig(level=logging.INFO)
 
 def main():
     run()
 
-def run(steerport='D', driveport='A', maxcontrol=100.0, fadezone=30.0, loopfreq=30.0):
+def run(steerport='A', driveport='B', maxcontrol=100.0, fadezone=30.0, loopfreq=30.0):
     """Operate a RC car with an xbox controller
 
     Args:
@@ -17,10 +19,22 @@ def run(steerport='D', driveport='A', maxcontrol=100.0, fadezone=30.0, loopfreq=
     import ev3local.xbox as xbox, ev3local.ev3 as ev3, ev3local.callback as controllers
     import time
 
-    with xbox.XCEvents() as xcevents, ev3.TachoMotor(steerport) as motor, ev3.TachoMotor(driveport) as drivemotor:
+    with xbox.XCEvents() as xcevents, ev3.TachoMotor(steerport) as motor, ev3.TachoMotor(driveport, rcmproperties=['Duty_Cycle_SP']) as drivemotor:
 
-        drivecontroller = controllers.DutyCycleController(xcevents, drivemotor)
+        # Start reading xbox events
+        #
+        xcevents.start()
 
+        # Connect an xbox axis to the drive motor
+        #
+        drivemotor.run_direct()
+        def d(v):
+            drivemotor.Duty_Cycle_SP = int(v)
+        xcevents.connectaxes(d, 'ABS_X', 100, -100)
+
+
+        # TODO: Use the PCntrl_TachoMotorPositionManager
+        #
         motor.reset()
         motor.run_direct()
 
@@ -29,11 +43,14 @@ def run(steerport='D', driveport='A', maxcontrol=100.0, fadezone=30.0, loopfreq=
                 kp, ev3local.pid.axissetpoint(xcevents),
                 ev3local.pid.processvariable(motor), ev3local.pid.clampedcontrol(motor, maxcontrol)) as controller:
             try:
+                controller.start()
                 while True:
                     time.sleep(1.0/loopfreq)
             finally:
                 motor.reset()
                 drivemotor.reset()
+                xcevents.stop()
+
 
 if __name__=='__main__':
     try:
